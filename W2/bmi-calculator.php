0af71e974a3d54e3f2aa6545c3086a0d0452f686
4cenzshikii <carlsonmartin24@gmail.com>
2026-01-22 12:18:04 +0800

    first commit

---
+++ W2/bmi-calculator.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html> 
+<html>
+
+<head>
+</head>
+<body>
+   <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+     <label for="">
+        Height (m)
+</label>
+    <input name="heightVal" type="number">
+    <label for="">
+        Weight (kg)
+</label>
+    <input name="weightVal" type="number">
+    <input type="submit" value="Calculate">
+    </form>  
+</body>
+<?php
+
+ if($_SERVER['REQUEST_METHOD']=="POST"){
+    $height = $_POST['heightVal'];
+    $weight = $_POST['weightVal'];
+
+    $bmi = $weight/(($height/100)* ($height/100));
+ }
+?>
+
+<?php
+echo $bmi;
+?>
+ </html>
---
+++ W2/play.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Carlson";
+echo "Hello World $name";
+echo 'This is Full Stack Class' $name;
+echo "<h1> My name is Carlson</h1>";
+echo "<h2> My PA name is Miss Athirah</h2>";
+echo "<h3> My Deparment Name Is JTMK</h3>";
+
+?>
